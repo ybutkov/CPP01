5087883a70b03a62a0cbe08c17bbc68c894077c9
Yurii Butkov <herr.butkov@gmail.com>
2026-02-08 23:17:08 +0100

    refactoring ex00

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,14 +6,27 @@
 /*   By: ybutkov <ybutkov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 15:03:29 by ybutkov           #+#    #+#             */
-/*   Updated: 2026/02/01 15:19:44 by ybutkov          ###   ########.fr       */
+/*   Updated: 2026/02/08 23:13:10 by ybutkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie* newZombie(std::string name);
-void    randomChump(std::string name);
+namespace 
+{
+
+void    randomChump(std::string name)
+{
+    Zombie zombie(name);
+    zombie.announce();
+}
+
+Zombie* newZombie(std::string name)
+{
+    return new Zombie(name);
+}
+
+} // endnamespce
 
 int main(void)
 {
